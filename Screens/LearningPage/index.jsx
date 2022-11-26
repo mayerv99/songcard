ef7d3ea4cf0c5fa508a8434f446f19f537bcb3a5
jmcauhy <jmcauhy@sempreceub.com.br>
2022-11-26 12:57:18 -0300

    Fetch and display selected song lyrics

--- Screens/LearningPage/index.jsx
+++ Screens/LearningPage/index.jsx
@@ -1,10 +1,55 @@
-import { SafeAreaView, Text } from "react-native";
-import React from "react";
+import { SafeAreaView, View, Text } from "react-native";
+import React, { useCallback, useEffect, useState } from "react";
+import api from '../../services/api'
 
-function LearningPage() {
+import useCurrentSong from "../../Context/Hooks/useCurrentSong";
+
+import { LyricsWrapper, Word } from "./styled";
+
+function LearningPage({ navigation }) {
+  const { currentSong } = useCurrentSong();
+
+  const [lyrics, setLyrics] = useState();
+
+  const getLyrics = useCallback(async () => {
+    if (currentSong === undefined)
+    {
+      return;
+    }
+
+    const endpoint = `/track.lyrics.get?track_id=${currentSong.track_id}&apikey=4306ade10d6239b3b17e0aadf07f0ff9`,
+          data = await api.get(endpoint).then(res => res.data),
+          lyrics = data?.message?.body?.lyrics?.lyrics_body;
+
+    if (lyrics?.length) {
+      const rawLyrics = lyrics.split('');
+      rawLyrics.splice(-75);
+      setLyrics(rawLyrics.join(''));
+      return;
+    }
+
+    setLyrics(null);
+  }, [currentSong]);
+
+  useEffect(() => {
+    getLyrics();
+  }, [getLyrics])
+
   return (
     <SafeAreaView style={{ backgroundColor: "white", minHeight: "100%" }}>
-      <Text>LearningPage</Text>
+      <LyricsWrapper onPress={() => currentSong === undefined && navigation.navigate("mainPage")}>
+        {currentSong === undefined && <Text>←  Volte e selecione uma música</Text>}
+        {lyrics?.length && lyrics?.split('\n').map((line, index) =>
+            <Text key={index}>
+              {line.split(' ').map((word, index) =>
+                <View key={index}>
+                  <Word>{word}</Word>
+                </View>
+              )}
+            </Text>
+        )}
+        {currentSong === null && <Text>Falha ao carregar letra da música.</Text>}
+      </LyricsWrapper>
     </SafeAreaView>
   );
 }
