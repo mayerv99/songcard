97bed7fe12558ceb278de4c5b5544812a61243c4
jmcauhy <jmcauhy@sempreceub.com.br>
2022-12-11 11:16:02 -0300

    temp

--- Context/Hooks/useCurrentUser.jsx
+++ Context/Hooks/useCurrentUser.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 
-import { AuthenticationContext } from "../AuthenticationContext";
+import { AuthenticationContext } from "../AuthenticationContext"
 
 export default function useCurrentUser() {
   const { currentUser, setCurrentUser } = useContext(AuthenticationContext);
